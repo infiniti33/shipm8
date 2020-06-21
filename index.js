c9be74e3966b1f367c651ef20115446256f6e882
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-06-21 15:51:30 -0700

    Moved App.js to src/index.js and updated imports

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src';
 import { name as appName } from './app.json';
 
-console.disableYellowBox = true;
+if (__DEV__) {
+  console.disableYellowBox = true;
+}
 
 AppRegistry.registerComponent(appName, () => App);
